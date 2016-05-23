2ce1ff38632115e6b8c08eab56816f813585a83a
石本利 <shibenli@vcredit.com>
2016-05-23 14:59:35 +0800

    添加调用demo

--- app/src/main/java/com/benli/randomkeyboard/MainActivity.java
+++ app/src/main/java/com/benli/randomkeyboard/MainActivity.java
@@ -2,6 +2,9 @@ package com.benli.randomkeyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.benli.keyboard.KeyboardHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new KeyboardHelper(this, (EditText) findViewById(R.id.edittext1));
     }
 }
